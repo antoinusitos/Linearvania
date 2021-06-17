7f9a93a52102c70784477629f4ea376fd85eb3f9
AntoineGargasson <antoine@warpfrog.com>
2021-06-17 20:24:50 +0200

    First push

---
+++ Source/Linearvania/LinearvaniaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LinearvaniaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINEARVANIA_API ALinearvaniaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
